c266fc0162ad333e6e48b34d142b6db186c249da
Yulingsong <yulingsong5@126.com>
2016-06-06 09:46:29 +0800

    Initial Commit

---
+++ webViewDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  webViewDemo2
+//
+//  Created by yulingsong on 16/6/6.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ webViewDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  webViewDemo2
+//
+//  Created by yulingsong on 16/6/6.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
